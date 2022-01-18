a972c9a3a1b20b914c98592cf77b10a03d7a9393
kumar.mayank <kumar.mayank@multisoup.co.jp>
2022-01-18 19:55:26 +0530

    Added Register Manager Screen

--- app/src/main/java/com/example/draggable/MainActivity.kt
+++ app/src/main/java/com/example/draggable/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.draggable
 import android.os.Bundle
 import android.view.Menu
 import android.view.View
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -13,6 +12,12 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.draggable.databinding.ActivityMainBinding
+import android.view.MenuItem
+import android.content.Intent
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -51,5 +56,22 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.getItemId()) {
+            R.id.register_settings -> {
+                startRegisterActivity()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun startRegisterActivity(){
+        val intent = Intent(this, RegisterManagerActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
---
+++ app/src/main/java/com/example/draggable/RegisterManagerActivity.kt
@@ -0,0 +1,31 @@
+package com.example.draggable
+
+import android.os.Bundle
+import android.view.Menu
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.example.draggable.databinding.ActivityScrollingBinding
+
+class RegisterManagerActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityScrollingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityScrollingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true);
+        binding.toolbarLayout.title = title
+       /* binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }*/
+    }
+
+
+}
